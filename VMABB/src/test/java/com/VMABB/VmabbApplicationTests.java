ce500907bf2e34ff847aaa9a35d3277bdfe7d5a9
Ashu Singh <ashu.singh2001.as@gmail.com>
2023-09-12 15:17:42 +0530

    Initial

---
+++ VMABB/src/test/java/com/VMABB/VmabbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VMABB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmabbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
